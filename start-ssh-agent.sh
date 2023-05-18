f37f896a12c36de40448d0c23bfcbb757929d1b1
cpsource <page.cal@gmail.com>
2023-05-18 08:11:38 -0400

    be sure to pkill ssh-agent

--- start-ssh-agent.sh
+++ start-ssh-agent.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # start this ssh-agent in the background
+
+# For W11, you must kill all old ssh-agents first
+# otherwise, you will have to enter the passphrase for your
+# key constantly
+pkill ssh-agent
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
