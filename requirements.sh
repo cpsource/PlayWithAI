f33fc78d9a27d37de112ab39e71bdf4f6d7011f7
cpsource <page.cal@gmail.com>
2023-05-20 05:51:41 -0400

    save and restore venv

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this is how we save a venv
+# pip freeze > requirements.txt
+
+# this is how we restore a venv
+# pip install -r requirements.txt
+
+# here's how we make a new venv
+# python -m venv venv
+
+# then to run it
+# source venv/bin/activate
