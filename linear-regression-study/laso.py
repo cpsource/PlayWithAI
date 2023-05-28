2e5cb49813bbce47494cb5ddfecce823c858e149
cpsource <page.cal@gmail.com>
2023-05-28 10:31:10 -0400

    linear regression with a real dataset

---
+++ linear-regression-study/laso.py
@@ -0,0 +1,33 @@
+# Author: Fabian Pedregosa <fabian.pedregosa@inria.fr>
+#         Alexandre Gramfort <alexandre.gramfort@inria.fr>
+# License: BSD 3 clause
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+from sklearn import datasets
+
+print(datasets.load_diabetes())
+
+X, y = datasets.load_diabetes(return_X_y=True)
+
+print(X)
+print(y)
+
+exit(0)
+
+print("Computing regularization path using the LARS ...")
+_, _, coefs = linear_model.lars_path(X, y, method="lasso", verbose=True)
+
+xx = np.sum(np.abs(coefs.T), axis=1)
+xx /= xx[-1]
+
+plt.plot(xx, coefs.T)
+ymin, ymax = plt.ylim()
+plt.vlines(xx, ymin, ymax, linestyle="dashed")
+plt.xlabel("|coef| / max|coef|")
+plt.ylabel("Coefficients")
+plt.title("LASSO Path")
+plt.axis("tight")
+plt.show()
