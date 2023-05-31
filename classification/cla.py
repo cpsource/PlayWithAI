21c12cf0430c709aec4f4d8cfa8afd00ac8a490b
cpsource <page.cal@gmail.com>
2023-05-31 04:29:21 -0400

    classification example

--- classification/cla.py
+++ classification/cla.py
@@ -174,6 +174,8 @@ list_of_metrics_to_plot = ['accuracy']
 
 plot_curve(epochs, hist, list_of_metrics_to_plot)
 
+# Evaluate the model against the test set
+
 features = {name:np.array(value) for name, value in test_df_norm.items()}
 label = np.array(features.pop(label_name))
 
@@ -188,4 +190,105 @@ my_model.evaluate(x = features, y = label, batch_size=batch_size)
 # accurate.
 
 # Note: Start at task3
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 100
+classification_threshold = 0.35
+label_name = "median_house_value_is_high"
+
+# Modify the following definition of METRICS to generate
+# not only accuracy and precision, but also recall:
+METRICS = [
+      tf.keras.metrics.BinaryAccuracy(name='accuracy', 
+                                      threshold=classification_threshold),
+      tf.keras.metrics.Precision(thresholds=classification_threshold,
+                                 name='precision' 
+                                 ),
+      tf.keras.metrics.Recall(thresholds=classification_threshold,
+                              name="recall"),
+  # ?  # write code here
+]
+
+# Establish the model's topography.
+my_model = create_model(inputs, learning_rate, METRICS)
+
+# Train the model on the training set.
+epochs, hist = train_model(my_model, train_df_norm, epochs, 
+                           label_name, batch_size)
+
+# Plot metrics vs. epochs
+list_of_metrics_to_plot = ['accuracy', 'precision', 'recall'] 
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+
+# Tassk 4 - Experiment with the classification threshold (if time permits)
+
+#@title Double-click to view the solution for Task 4.
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 100
+classification_threshold = 0.52
+label_name = "median_house_value_is_high"
+
+# Here is the updated definition of METRICS:
+METRICS = [
+      tf.keras.metrics.BinaryAccuracy(name='accuracy',
+                                      threshold=classification_threshold),
+      tf.keras.metrics.Precision(thresholds=classification_threshold,
+                                 name='precision'
+                                 ),
+      tf.keras.metrics.Recall(thresholds=classification_threshold,
+                              name="recall"),
+]
+
+# Establish the model's topography.
+my_model = create_model(inputs, learning_rate, METRICS)
+
+# Train the model on the training set.
+epochs, hist = train_model(my_model, train_df_norm, epochs,
+                           label_name, batch_size)
+
+# Plot metrics vs. epochs
+list_of_metrics_to_plot = ['accuracy', "precision", "recall"]
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+
+# A `classification_threshold` of slightly over 0.5
+# appears to produce the highest accuracy (about 83%).
+# Raising the `classification_threshold` to 0.9 drops
+# accuracy by about 5%.  Lowering the
+# `classification_threshold` to 0.3 drops accuracy by
+# about 3%.
+
+# Task 5 - Summarize model  performace (if time permits)
+
+#@title Double-click to view the solution for Task 5.
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 100
+label_name = "median_house_value_is_high"
+
+# AUC is a reasonable "summary" metric for
+# classification models.
+# Here is the updated definition of METRICS to
+# measure AUC:
+METRICS = [
+      tf.keras.metrics.AUC(num_thresholds=100, name='auc'),
+]
+
+# Establish the model's topography.
+my_model = create_model(inputs, learning_rate, METRICS)
+
+# Train the model on the training set.
+epochs, hist = train_model(my_model, train_df_norm, epochs,
+                           label_name, batch_size)
+
+# Plot metrics vs. epochs
+list_of_metrics_to_plot = ['auc']
+plot_curve(epochs, hist, list_of_metrics_to_plot)
+
 
