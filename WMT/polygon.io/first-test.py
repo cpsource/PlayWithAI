aeff49a3d2394fe3be15e00d506a98ccf3719b3f
Cal Page <page.cal@gmail.com>
2023-07-13 09:54:51 -0400

    look at polygon data

---
+++ WMT/polygon.io/first-test.py
@@ -0,0 +1,37 @@
+from polygon import RESTClient
+import capikey
+#client = RESTClient(api_key="<API_KEY>")
+client = capikey.client
+
+print(client)
+
+ticker = "AAPL"
+
+if False:
+    # List Aggregates (Bars)
+    aggs = []
+    for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
+        aggs.append(a)
+    print(aggs)
+
+if False:
+    # Get Last Trade
+    trade = client.get_last_trade(ticker=ticker)
+    print(trade)
+
+if True:
+    # List Trades
+    trades = client.list_trades(ticker=ticker, timestamp="2023-01-04")
+    for trade in trades:
+        print(trade)
+
+if False:
+    # Get Last Quote
+    quote = client.get_last_quote(ticker=ticker)
+    print(quote)
+
+if False:
+    # List Quotes
+    quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
+    for quote in quotes:
+        print(quote)
