b946f117c2417a2cb7c31645a648453abc6e35d0
Cal Page <page.cal@gmail.com>
2023-07-20 09:12:25 -0400

    refine startup scripts and nedw requirements.txt

--- setup-venv.sh
+++ setup-venv.sh
@@ -4,6 +4,8 @@ source ~/venv/bin/activate
 # get TensorFlow setup properly
 CUDNN_PATH=$(dirname $(python3 -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 export LD_LIBRARY_PATH=$CUDNN_PATH/lib:$LD_LIBRARY_PATH
+# reduce TensorFlow logging level
+ export TF_CPP_MIN_LOG_LEVEL=3
 # display some things
 ./nvcc.sh
 ./verify-tensor-flow.sh
