048b62501b00c424efb3026d615d87709aee16dc
Cal Page <page.cal@gmail.com>
2023-07-20 09:31:09 -0400

    reorg a bit

--- setup-venv.sh
+++ setup-venv.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # get to right python venv
 source ~/venv/bin/activate
+# set POLYGON_API_KEY
+export POLYGON_API_KEY=`cat finance/polygon.io/apikey.txt`
 # get TensorFlow setup properly
 CUDNN_PATH=$(dirname $(python3 -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))
 export LD_LIBRARY_PATH=$CUDNN_PATH/lib:$LD_LIBRARY_PATH
 # reduce TensorFlow logging level
  export TF_CPP_MIN_LOG_LEVEL=3
-# display some things
+# display some things here and there
 ./nvcc.sh
 ./verify-tensor-flow.sh
